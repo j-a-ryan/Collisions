2aed9e6acfb48d840f0679f81d18779819f0f462
Jim Ryan <j.a.ryan@protonmail.com>
2026-04-07 22:20:28 -0400

    Factored out arg type checkboxes

--- view/plot_view/widgets.py
+++ view/plot_view/widgets.py
@@ -17,7 +17,7 @@ class ConfigureTransformationPopup(QDialog):
         layout = QVBoxLayout()
         layout.setAlignment(Qt.AlignTop)
         
-        vectors_label = QLabel("Vectors:")
+        vectors_label = QLabel("Vectors")
         vectors_label_font = QLabel().font()
         vectors_label_font.setItalic(True)
         vectors_label_font.setPointSize(12)
@@ -33,7 +33,7 @@ class ConfigureTransformationPopup(QDialog):
         self.vector_pair_label.setFont(label_top_center_font)
         
         V_Y_hbox = QHBoxLayout()
-        V_Y_hbox.setSpacing(0)
+        V_Y_hbox.setSpacing(5)
         swap_button = QPushButton("Swap V \u2194 Y")
         swap_button.setFixedWidth(100)
         swap_button.clicked.connect(self.on_reversed)
@@ -42,52 +42,17 @@ class ConfigureTransformationPopup(QDialog):
         V_Y_hbox.addWidget(swap_button)
         V_Y_hbox.addStretch()
         layout.addLayout(V_Y_hbox)
+        layout.addSpacing(25)
 
-        config_args_label = QLabel("Configuration arguments:")
-        config_args_label_font = QLabel().font()
-        config_args_label_font.setItalic(True)
-        config_args_label_font.setPointSize(12)
-        config_args_label.setFont(config_args_label_font)
-        self.rest_frame_checkboxes_group = QButtonGroup()
-        self.rest_frame_checkboxes_group.setExclusive(True)
-        self.V_rest_frame_checkbox = QCheckBox("(V, Y)")
-        self.V_rest_frame_checkbox.setChecked(True)
-        self.V_plus_Y_rest_frame_checkbox = QCheckBox("(V + Y, Y)")
-        self.V_plus_Y_rest_frame_checkbox.checkStateChanged.connect(self.two_step_transformation_check)
-        self.rest_frame_checkboxes_group.addButton(self.V_rest_frame_checkbox)
-        self.rest_frame_checkboxes_group.addButton(self.V_plus_Y_rest_frame_checkbox)
-        layout.addSpacing(15)
-        layout.addWidget(config_args_label, alignment=Qt.AlignmentFlag.AlignHCenter)
-        
-        
-        layout.addWidget(self.V_rest_frame_checkbox)
-        hbox_arguments = QHBoxLayout()
-        hbox_arguments.setSpacing(0)
-        hbox_arguments.addWidget(self.V_plus_Y_rest_frame_checkbox, alignment=Qt.AlignmentFlag.AlignLeft)
-        self.post_transformation_checkbox = QCheckBox("(V' - Y', Y)")
-        self.post_transformation_checkbox.setEnabled(False)
-        arrow_label = QLabel("\u2192")
-        arrow_label_font = arrow_label.font()
-        # arrow_label_font.setItalic(True)
-        arrow_label_font.setPointSize(16)
-        arrow_label.setFont(arrow_label_font)
-        hbox_arguments.addWidget(arrow_label, alignment=Qt.AlignmentFlag.AlignHCenter)
-        hbox_arguments.addWidget(self.post_transformation_checkbox, alignment=Qt.AlignmentFlag.AlignRight)
-        layout.addLayout(hbox_arguments)
-        layout.addWidget(self.post_transformation_checkbox)
+        create_argument_type_checkboxes(self, layout)
+
+        # layout.addLayout(self.arg_type_checkboxes)
 
         self.submit_cancel_button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
         self.submit_cancel_button_box.accepted.connect(self.accept)
         self.submit_cancel_button_box.rejected.connect(self.reject)
         layout.addWidget(self.submit_cancel_button_box, alignment=Qt.AlignmentFlag.AlignRight)
         self.setLayout(layout)
-
-    def two_step_transformation_check(self):
-        if self.V_plus_Y_rest_frame_checkbox.isChecked():
-            self.post_transformation_checkbox.setEnabled(True)
-        else:
-            self.post_transformation_checkbox.setChecked(False)
-            self.post_transformation_checkbox.setEnabled(False)
 
     def on_reversed(self):
         if self.vector_pair_label.text() == self.pairing_text_base_case:
@@ -102,7 +67,8 @@ class ConfigureTransformationPopup(QDialog):
         else:
             self.transformation_config["V"] = self.second_particle
             self.transformation_config["Y"] = self.first_particle
-        self.transformation_config["V+YConfig"] = self.V_plus_Y_rest_frame_checkbox.isChecked()
+        self.transformation_config["VConfig"] = self.V_Y_argument_type_checkbox.isChecked()
+        self.transformation_config["V+YConfig"] = self.V_plus_Y_argument_type_checkbox.isChecked()
         self.transformation_config["ApplyPostTransformationV'-Y'"] = self.post_transformation_checkbox.isChecked()
         
         super().accept()
@@ -112,6 +78,48 @@ class ConfigureTransformationPopup(QDialog):
             self.vector_pair_label.setText(self.pairing_text_switched)
         else:
             self.vector_pair_label.setText(self.pairing_text_base_case)
+    
+    def two_step_transformation_check(self):
+        if self.V_plus_Y_argument_type_checkbox.isChecked():
+            self.post_transformation_checkbox.setEnabled(True)
+        else:
+            self.post_transformation_checkbox.setChecked(False)
+            self.post_transformation_checkbox.setEnabled(False)
+
+def create_argument_type_checkboxes(parent_form, parent_form_vbox_layout):
+
+    config_args_label = QLabel("Configuration arguments")
+    config_args_label_font = QLabel().font()
+    config_args_label_font.setItalic(True)
+    config_args_label_font.setPointSize(12)
+    config_args_label.setFont(config_args_label_font)
+    parent_form.argument_types_checkboxes_group = QButtonGroup()
+    parent_form.argument_types_checkboxes_group.setExclusive(True)
+    parent_form.V_Y_argument_type_checkbox = QCheckBox("(V, Y)")
+    parent_form.V_Y_argument_type_checkbox.setChecked(True)
+    parent_form.V_plus_Y_argument_type_checkbox = QCheckBox("(V + Y, Y)")
+    parent_form.V_plus_Y_argument_type_checkbox.checkStateChanged.connect(parent_form.two_step_transformation_check)
+    parent_form.argument_types_checkboxes_group.addButton(parent_form.V_Y_argument_type_checkbox)
+    parent_form.argument_types_checkboxes_group.addButton(parent_form.V_plus_Y_argument_type_checkbox)
+    
+    parent_form_vbox_layout.addWidget(config_args_label, alignment=Qt.AlignmentFlag.AlignHCenter)
+    
+    parent_form_vbox_layout.addWidget(parent_form.V_Y_argument_type_checkbox)
+    hbox_arguments = QHBoxLayout()
+    hbox_arguments.setSpacing(0)
+    hbox_arguments.addWidget(parent_form.V_plus_Y_argument_type_checkbox, alignment=Qt.AlignmentFlag.AlignLeft)
+    parent_form.post_transformation_checkbox = QCheckBox("(V' - Y', Y)")
+    parent_form.post_transformation_checkbox.setEnabled(False)
+    arrow_label = QLabel("\u2192")
+    arrow_label_font = arrow_label.font()
+    # arrow_label_font.setItalic(True)
+    arrow_label_font.setPointSize(16)
+    arrow_label.setFont(arrow_label_font)
+    hbox_arguments.addWidget(arrow_label, alignment=Qt.AlignmentFlag.AlignHCenter)
+    hbox_arguments.addWidget(parent_form.post_transformation_checkbox, alignment=Qt.AlignmentFlag.AlignRight)
+    parent_form_vbox_layout.addLayout(hbox_arguments)
+
+    
             
 if __name__ == "__main__":
     app = QApplication(sys.argv)
